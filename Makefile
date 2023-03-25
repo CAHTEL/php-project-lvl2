07964f7e235d2fa32da4c00590e206db1c2d5f74
Emil <emil8767_11@mail.ru>
2023-03-26 01:25:34 +0700

    Alersandru

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ install:
 	composer install
 validate:
 	composer validate
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
+test:
+	composer exec --verbose phpunit tests
---
+++ tests/gendiffTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\GendiffTest;
+
+use PHPUnit\Framework\TestCase;
+use function Differ\Differ\genDiff;
+
+class GendiffTest extends TestCase
+{
+    public function testGendiff(): void
+    {
+        $file1 = __DIR__ . "fixtures/file1.json";
+        $file2 = __DIR__ . "fixtures/file2.json";
+        $result = file_get_contents(__DIR__ . "tests/fixtures/result.json", true);
+        $this->assertJsonFileEqualsJsonFile($result, genDiff($file1, $file2));
+    }
+}
