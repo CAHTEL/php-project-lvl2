7246c9a6b20a8badcf90a05cff787822a1b80d13
Emil <emil8767_11@mail.ru>
2023-04-01 14:05:17 +0700

    3

--- src/Differ.php
+++ src/Differ.php
@@ -4,6 +4,7 @@ namespace Differ\Differ;
 
 function genDiff($file1, $file2)
 {
+    if(!is_array($file1) && !is_array($file2)) {
     $data1 = file_get_contents($file1, true);
     $data2 = file_get_contents($file2, true);
     $firstFile = json_decode($data1, true);
@@ -18,13 +19,44 @@ function genDiff($file1, $file2)
         if (array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile) && $firstFile[$key] === $secondFile[$key]) {
             $result[" " . $key] = $value;
         }
-        if (array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile) && $firstFile[$key] !== $secondFile[$key]) {
+        if (array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile) && $firstFile[$key] !== $secondFile[$key] && !is_array($value)) {
             $result["-" . $key] = $firstFile[$key];
             $result["+" . $key] = $secondFile[$key];
         }
         if (array_key_exists($key, $secondFile) && !array_key_exists($key, $firstFile)) {
             $result["+" . $key] = $value;
         }
+        if (array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile) && $firstFile[$key] !== $secondFile[$key] && is_array($value)) {
+            $result[" " . $key] = genDiff($firstFile[$key], $secondFile[$key]);
+        }
+        }
+    
+    return json_encode($result, JSON_PRETTY_PRINT); }
+    
+    if (is_array($file1) && is_array($file2)) {
+        $data = array_merge($file1, $file2);
+        ksort($data);
+        $result = [];
+        foreach ($data as $key => $value) {
+            if (array_key_exists($key, $file1) && !array_key_exists($key, $file2)) {
+                $result["-" . $key] = $value;
+            }
+            if (array_key_exists($key, $file1) && array_key_exists($key, $file2) && $file1[$key] === $file2[$key]) {
+                $result[" " . $key] = $value;
+            }
+            if (array_key_exists($key, $file1) && array_key_exists($key, $file2) && $file1[$key] !== $file2[$key] && !is_array($value)) {
+                $result["-" . $key] = $file1[$key];
+                $result["+" . $key] = $file2[$key];
+            }
+            if (array_key_exists($key, $file2) && !array_key_exists($key, $file1)) {
+                $result["+" . $key] = $value;
+            }
+            if (array_key_exists($key, $file1) && array_key_exists($key, $file2) && $file1[$key] !== $file2[$key] && is_array($value)) {
+                $result[" " . $key] = genDiff($file1[$key], $file2[$key]);
+            }
+            }
+        
+        return $result; 
     }
-    return json_encode($result, JSON_PRETTY_PRINT);
-}
+
+    }
--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -9,9 +9,16 @@ class GendiffTest extends TestCase
 {
     public function testGendiff(): void
     {
-        $file1 = __DIR__ . "/fixtures/file1.json";
-        $file2 = __DIR__ . "/fixtures/file2.json";
-        $result = file_get_contents(__DIR__ . "/fixtures/result.json", true);
-        $this->assertEquals($result, genDiff($file1, $file2));
+        $file1Json = __DIR__ . "/fixtures/file1.json";
+        $file2Json = __DIR__ . "/fixtures/file2.json";
+        $result1 = file_get_contents(__DIR__ . "/fixtures/result1.json", true);
+        $this->assertEquals($result1, genDiff($file1Json, $file2Json));
+        $file1Yml = __DIR__ . "/fixtures/file1.yml";
+        $file2Yml = __DIR__ . "/fixtures/file2.yml";
+        $this->assertEquals($result1, gendiff($file1Yml, $file2Yml));
+        $file3Json = __DIR__ . "/fixtures/file3.json";
+        $file4Json = __DIR__ . "/fixtures/file4.json";
+        $result2 = file_get_contents(__DIR__ . "/fixtures/result2.json", true);
+        $this->assertEquals($result2, gendiff($file3Json, $file4Json));
     }
 }
